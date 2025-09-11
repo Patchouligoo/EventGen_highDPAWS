679d7ea02b2ebee68614d117e720a57163e1dcee
“RunzeLi” <runze.li@yale.edu>
2025-09-11 08:16:24 -0700

    [dev] add task to collect all final outputs, also update read me

--- README.md
+++ README.md
@@ -18,16 +18,32 @@ To generate signals at one mass
 law run OmniLearnSignalPrep --version eventgen_production --mx 300 --my 300 --process qq --n-events 1000 --cluster-mode slurm (or local)
 ```
 
-To generate all signal mass grids used in PAWS study
+To get all inputs for Omnilearn PAWS, run each of the following 3 steps:
+
+1. Generate all signal mass grids used in PAWS study
 
 first run
 ```
-law run SubmitSignalsAllMass --version eventgen_production --process qq --n-events 500000 --cluster-mode slurm --workers 144
+sbatch slurm_submission.sh
 ```
-needs to be run on perlmutter cpu node with slurm. This will do all the delphes + pythia works to get root files
+This will do all the delphes + pythia works to get root files on slurm
 
 then run
 ```
 law run OmniLearnSignalPrepAllMass --version eventgen_production --process qq --n-events 500000 --workers 10 
 ```
-to further process outputs in Ominlearn format
+to further process outputs in Ominlearn format, need ~ 200 GB of memory, recommanded to run on perlmutter cpu node
+
+2. Generate backgrounds, run:
+```
+law run BackgroundProcessing --version eventgen_production --data-type qcd --region SR
+law run BackgroundProcessing --version eventgen_production --data-type qcd --region SB
+law run BackgroundProcessing --version eventgen_production --data-type extra_qcd --region SR
+```
+Need ~ 300 GB of memory, recommanded to run on perlmutter cpu node
+
+3. For final outputs together, run:
+```
+law run FinalOutput --version eventgen_production
+```
+Need ~ 128GB of memory, recommanded to run on perlmutter cpu node
---
+++ tasks/final_output.py
@@ -0,0 +1,218 @@
+import luigi
+import law
+import pandas as pd
+import numpy as np
+import h5py as h5
+from tqdm import tqdm
+
+from tasks.signal_generation import OmniLearnSignalPrep, NEventsMixin, DecayChannelMixin, BaseTask
+from tasks.bkg_generation import BackgroundProcessing
+
+class FinalOutput(
+    BaseTask,
+):
+    
+    pad_size = luigi.IntParameter(default=150)
+    mx_values = np.linspace(50, 600, 12)
+    my_values = np.linspace(50, 600, 12)
+
+    def requires(self):
+
+        reqs = {}
+        for mx in self.mx_values:
+            for my in self.my_values:
+                req = OmniLearnSignalPrep.req(
+                    self,
+                    mx=mx,
+                    my=my,
+                    n_events=500000,
+                    process="qq"
+                )
+                key = f"mx_{mx}_my_{my}"
+                reqs[key] = req
+        
+        reqs["SR_data_bkg"] = BackgroundProcessing.req(
+            self,
+            data_type="qcd",
+            feature_level="low_level",
+            region="SR",
+        )
+        reqs["SB_data_bkg"] = BackgroundProcessing.req(
+            self,
+            data_type="qcd",
+            feature_level="low_level",
+            region="SB",
+        )
+        reqs["SR_MC_bkg"] = BackgroundProcessing.req(
+            self,
+            data_type="extra_qcd",
+            feature_level="low_level",
+            region="SR",
+        )
+        
+        return reqs
+
+    def output(self):
+        return {
+            # used to form pseudo data, use bkg from LHCO original samples
+            "SR_data_bkg": self.local_target(f"SR_data_bkg.h5"),
+            "SR_data_sig": self.local_target(f"SR_data_sig.h5"),
+            "SB_data_bkg": self.local_target(f"SB_data_bkg.h5"),
+
+            # used to form MC simulation, use bkg from extra QCD samples
+            "SR_MC_bkg": self.local_target(f"SR_MC_bkg.h5"),
+            "SR_MC_sig": self.local_target(f"SR_MC_sig.h5"),
+        }
+    
+    @law.decorator.safe_output
+    def run(self):
+
+        np.random.seed(42)
+
+        # --------------------- signals for data ---------------------
+        df_SR_data_sig_constituents = None
+        df_SR_data_sig_global = None
+        df_SR_data_sig_condition = None
+        df_SR_data_sig_param_mx = None
+        df_SR_data_sig_param_my = None
+
+        pbar = tqdm(self.mx_values, desc="Processing signals for data")
+        for mx in pbar:
+            for my in self.my_values:
+                key = f"mx_{mx}_my_{my}"
+                SR_data_sig_path_mxmy = self.input()[key]["signals_for_data"].path
+
+                if df_SR_data_sig_constituents is None:
+                    with h5.File(SR_data_sig_path_mxmy, "r") as hf:
+                        df_SR_data_sig_constituents = hf["constituents"][:]
+                        df_SR_data_sig_global = hf["global"][:]
+                        df_SR_data_sig_condition = hf["condition"][:]
+                    
+                        df_SR_data_sig_param_mx = np.full(hf["constituents"][:].shape[0], mx)
+                        df_SR_data_sig_param_my = np.full(hf["constituents"][:].shape[0], my)
+
+                else:
+                    with h5.File(SR_data_sig_path_mxmy, "r") as hf:
+                        df_SR_data_sig_constituents = np.concatenate(
+                            (df_SR_data_sig_constituents, hf["constituents"][:]), axis=0
+                        )
+                        df_SR_data_sig_global = np.concatenate(
+                            (df_SR_data_sig_global, hf["global"][:]), axis=0
+                        )
+                        df_SR_data_sig_condition = np.concatenate(
+                            (df_SR_data_sig_condition, hf["condition"][:]), axis=0
+                        )
+                    
+                        df_SR_data_sig_param_mx = np.concatenate(
+                            (df_SR_data_sig_param_mx, np.full(hf["constituents"][:].shape[0], mx)), axis=0
+                        )
+                        df_SR_data_sig_param_my = np.concatenate(
+                            (df_SR_data_sig_param_my, np.full(hf["constituents"][:].shape[0], my)), axis=0
+                        )
+
+        print("Total number of available signal events for data: ", df_SR_data_sig_constituents.shape[0])
+
+        # shuffle signals for data
+        indices = np.arange(df_SR_data_sig_constituents.shape[0])
+        np.random.shuffle(indices)
+        df_SR_data_sig_constituents = df_SR_data_sig_constituents[indices]
+        df_SR_data_sig_global = df_SR_data_sig_global[indices]
+        df_SR_data_sig_condition = df_SR_data_sig_condition[indices]
+        df_SR_data_sig_param_mx = df_SR_data_sig_param_mx[indices]
+        df_SR_data_sig_param_my = df_SR_data_sig_param_my[indices]
+
+        # store signals for data
+        self.output()["SR_data_sig"].parent.touch()
+        with h5.File(self.output()["SR_data_sig"].path, "w") as hf:
+            hf.create_dataset("constituents", data=df_SR_data_sig_constituents)
+            hf.create_dataset("global", data=df_SR_data_sig_global)
+            hf.create_dataset("condition", data=df_SR_data_sig_condition)
+            hf.create_dataset("param_mx", data=df_SR_data_sig_param_mx)
+            hf.create_dataset("param_my", data=df_SR_data_sig_param_my)
+
+
+        # --------------------- signals for MC ---------------------
+        df_SR_MC_sig_constituents = None
+        df_SR_MC_sig_global = None
+        df_SR_MC_sig_condition = None
+        df_SR_MC_sig_param_mx = None
+        df_SR_MC_sig_param_my = None
+
+        pbar = tqdm(self.mx_values, desc="Processing signals for MC")
+        for mx in pbar:
+            for my in self.my_values:
+                key = f"mx_{mx}_my_{my}"
+                SR_MC_sig_path_mxmy = self.input()[key]["signals_for_mc"].path
+
+                if df_SR_MC_sig_constituents is None:
+                    with h5.File(SR_MC_sig_path_mxmy, "r") as hf:
+                        df_SR_MC_sig_constituents = hf["constituents"][:]
+                        df_SR_MC_sig_global = hf["global"][:]
+                        df_SR_MC_sig_condition = hf["condition"][:]
+                    
+                        df_SR_MC_sig_param_mx = np.full(hf["constituents"][:].shape[0], mx)
+                        df_SR_MC_sig_param_my = np.full(hf["constituents"][:].shape[0], my)
+
+                else:
+                    with h5.File(SR_MC_sig_path_mxmy, "r") as hf:
+                        df_SR_MC_sig_constituents = np.concatenate(
+                            (df_SR_MC_sig_constituents, hf["constituents"][:]), axis=0
+                        )
+                        df_SR_MC_sig_global = np.concatenate(
+                            (df_SR_MC_sig_global, hf["global"][:]), axis=0
+                        )
+                        df_SR_MC_sig_condition = np.concatenate(
+                            (df_SR_MC_sig_condition, hf["condition"][:]), axis=0
+                        )
+                    
+                        df_SR_MC_sig_param_mx = np.concatenate(
+                            (df_SR_MC_sig_param_mx, np.full(hf["constituents"][:].shape[0], mx)), axis=0
+                        )
+                        df_SR_MC_sig_param_my = np.concatenate(
+                            (df_SR_MC_sig_param_my, np.full(hf["constituents"][:].shape[0], my)), axis=0
+                        )
+
+        print("Total number of available signal events for MC: ", df_SR_MC_sig_constituents.shape[0])
+        # shuffle signals for MC
+        indices = np.arange(df_SR_MC_sig_constituents.shape[0])
+        np.random.shuffle(indices)
+        df_SR_MC_sig_constituents = df_SR_MC_sig_constituents[indices]
+        df_SR_MC_sig_global = df_SR_MC_sig_global[indices]
+        df_SR_MC_sig_condition = df_SR_MC_sig_condition[indices]
+        df_SR_MC_sig_param_mx = df_SR_MC_sig_param_mx[indices]
+        df_SR_MC_sig_param_my = df_SR_MC_sig_param_my[indices]
+
+        # store signals for MC
+        with h5.File(self.output()["SR_MC_sig"].path, "w") as hf:
+            hf.create_dataset("constituents", data=df_SR_MC_sig_constituents)
+            hf.create_dataset("global", data=df_SR_MC_sig_global)
+            hf.create_dataset("condition", data=df_SR_MC_sig_condition)
+            hf.create_dataset("param_mx", data=df_SR_MC_sig_param_mx)
+            hf.create_dataset("param_my", data=df_SR_MC_sig_param_my)
+
+        # --------------------- backgrounds for data ---------------------
+        SR_data_bkg_path = self.input()["SR_data_bkg"].path
+        # No need to do any processing, just copy the file
+        with h5.File(SR_data_bkg_path, "r") as hf_in:
+            with h5.File(self.output()["SR_data_bkg"].path, "w") as hf_out:
+                for key in hf_in.keys():
+                    hf_out.create_dataset(key, data=hf_in[key][:])
+                print("Total number of available background events for data in SR: ", hf_in["constituents"][:].shape[0])
+
+        SB_data_bkg_path = self.input()["SB_data_bkg"].path
+        # No need to do any processing, just copy the file
+        with h5.File(SB_data_bkg_path, "r") as hf_in:
+            with h5.File(self.output()["SB_data_bkg"].path, "w") as hf_out:
+                for key in hf_in.keys():
+                    hf_out.create_dataset(key, data=hf_in[key][:])
+                print("Total number of available background events for data in SB: ", hf_in["constituents"][:].shape[0])
+
+        # --------------------- backgrounds for MC ---------------------
+        SR_MC_bkg_path = self.input()["SR_MC_bkg"].path
+        # No need to do any processing, just copy the file
+        with h5.File(SR_MC_bkg_path, "r") as hf_in:
+            with h5.File(self.output()["SR_MC_bkg"].path, "w") as hf_out:
+                for key in hf_in.keys():
+                    hf_out.create_dataset(key, data=hf_in[key][:])
+                print("Total number of available background events for MC in SR: ", hf_in["constituents"][:].shape[0])
+
